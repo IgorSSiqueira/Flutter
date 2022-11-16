9a6dd149b7ad2531ba4383df2cf98c97f0e6c606
IgorSSiqueira <igor.ss@live.com>
2022-11-16 09:04:23 -0300

    16-11-2022

---
+++ Ensinamentos/float_action_button/lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:float_action_button/Home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(home: Home()));
+}
+
--- Ensinamentos/manipulacao_dados_preferencias/lib/Home.dart
+++ Ensinamentos/manipulacao_dados_preferencias/lib/Home.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,14 +12,29 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
 
+  String _textoSalvo = 'Nada salvo!';
   TextEditingController _controllerCampo = TextEditingController();
 
-  _salvar(){
+  _salvar() async {
+    String valorDigitado = _controllerCampo.text;
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('nome', valorDigitado);
 
+    print ('Salvar: $valorDigitado');
   }
 
-  _recuperar(){
+  _recuperar() async {
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _textoSalvo = prefs.getString('nome') ?? 'Sem valor';
+    });
+    print('Recuperar $_textoSalvo');
+  }
 
+  _remover() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.remove('nome');
+    print('Removeu');
   }
 
   @override
@@ -32,7 +48,7 @@ class _HomeState extends State<Home> {
         child: Column(
           children: [
             Text(
-              'Nada salvo!',
+              _textoSalvo,
               style: TextStyle(fontSize: 20),
             ),
             TextField(
@@ -42,40 +58,49 @@ class _HomeState extends State<Home> {
               ),
               controller: _controllerCampo,
             ),
+            SizedBox(height: 20),
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                Padding(
-                    padding: EdgeInsets.all(15),
-                  child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                          backgroundColor: Colors.blue,
-                          textStyle: TextStyle(
-                            color: Colors.white,
-                          )
-                      ),
-                      onPressed: () => _salvar(),
-                      child: Text(
-                        'Salvar',
-                        style: TextStyle(fontSize: 20),
-                      ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                      )
+                  ),
+                  onPressed: () => _salvar(),
+                  child: Text(
+                    'Salvar',
+                    style: TextStyle(fontSize: 20),
                   ),
                 ),
-                Padding(
-                  padding: EdgeInsets.all(15),
-                  child: ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.blue,
-                        textStyle: TextStyle(
-                          color: Colors.white,
-                        )
-                    ),
-                    onPressed: () => _recuperar(),
-                    child: Text(
-                      'Ler',
-                      style: TextStyle(fontSize: 20),
-                    ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                      )
                   ),
-                )
+                  onPressed: () => _recuperar(),
+                  child: Text(
+                    'Ler',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      textStyle: TextStyle(
+                        color: Colors.white,
+                      )
+                  ),
+                  onPressed: () => _remover(),
+                  child: Text(
+                    'Remover',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
               ],
             )
           ],
