65f0beee6a0e9411ad5faf486422d54c87e4bf5c
Igor S Siqueira <igor.ss@live.com>
2022-11-28 21:01:32 -0300

    28-11-2022 21:00

--- CopiasLayouts/layout_santander/lib/home.dart
+++ CopiasLayouts/layout_santander/lib/home.dart
@@ -12,7 +12,7 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color(0xFFE1453A),
+        backgroundColor: const Color(0xFFDE3E33),
         elevation: 0,
         automaticallyImplyLeading: false,
         actions: [
@@ -38,25 +38,61 @@ class _HomeState extends State<Home> {
           child: Column(
         children: [
           Container(
-            height: 125,
-            color: Color(0xFFE1453A),
+            height: 110,
+            color: const Color(0xFFDE3E33),
             child: Column(children: [
               Padding(
-                padding: EdgeInsets.all(20),
+                padding: const EdgeInsets.all(16),
                 child: Column(
                   children: [
-                    SizedBox(height: 0),
-                    Row(children: [
+                    Row(children: const [
                       Text(
                         'Olá, hugão',
-                        style: TextStyle(fontSize: 20),
+                        style: TextStyle(fontSize: 20, color: Colors.white),
                       )
                     ]),
-                    SizedBox(height: 10),
-                    Row()
+                    const SizedBox(height: 10),
+                    Row(children: const [
+                      Text(
+                        'Ag 4321',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      ),
+                      SizedBox(width: 10),
+                      Text(
+                        'CC 654321-0',
+                        style: TextStyle(fontSize: 16, color: Colors.white),
+                      )
+                    ]),
                   ],
                 ),
               ),
+            ]),
+          ),
+          const SizedBox(height: 70),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white, border: Border.all(width: 8)),
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: 140,
+            color: Colors.white,
+            child: Column(children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(12),
+                    child: Row(children: [
+                      const Icon(Icons.attach_money),
+                      const SizedBox(width: 10),
+                      const Text(
+                        'Saldo disponível',
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ]),
+                  ),
+                  const Icon(Icons.keyboard_arrow_up, size: 30)
+                ],
+              )
             ]),
           )
         ],
