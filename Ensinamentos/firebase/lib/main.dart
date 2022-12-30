00cf10b87c10a06d0a2b3d649ceec7122b6b51a5
IgorSSiqueira <igor.ss@live.com>
2022-12-30 09:03:18 -0300

    flutter authentication

--- Ensinamentos/firebase/lib/main.dart
+++ Ensinamentos/firebase/lib/main.dart
@@ -2,12 +2,13 @@
 
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 void main() async {
   //Inicializar Firebase
   WidgetsFlutterBinding.ensureInitialized();
 
-  Firestore db = Firestore.instance;
+  //Firestore db = Firestore.instance;
 
   /*db.collection('usuarios').document('002').setData({
     'nome': 'Ana Maria Silva',
@@ -54,23 +55,43 @@ void main() async {
       var dados = item.data;
       print('dados usuarios: ${dados['nome']} - ${dados['idade']}');
     }
-  });*/
-
+  });
   //FILTROS
   QuerySnapshot querySnapshot = await db
       .collection('usuarios')
       //.where('nome', isEqualTo: 'teste')
       //.where('idade', isEqualTo: 25)
-      .where('idade', isGreaterThan: 15)
+      //.where('idade', isGreaterThan: 15)
       //.where('idade', isLessThan: 35)
-      .orderBy('idade', descending: true)
-      .orderBy('nome', descending: false)
+      //.orderBy('idade', descending: true)
+      //.orderBy('nome', descending: false)
+      //.limit(2)
+      .where('nome', arrayContains: 'ig')
       .getDocuments();
 
   for (DocumentSnapshot item in querySnapshot.documents) {
     var dados = item.data;
     print('Filtro nome: ${dados['nome']} idade: ${dados['idade']}');
-  }
+  }*/
+
+  /* ****************************************************************** */
+  /* *************************** AULA 154 + *************************** */
+  /* ****************************************************************** */
+
+  FirebaseAuth auth = FirebaseAuth.instance;
+
+  /* CRIANDO USUÁRIO COM EMAIL E SENHA */
+
+  String email = 'igor@gmail.com';
+  String senha = '123456';
+
+  auth
+      .createUserWithEmailAndPassword(email: email, password: senha)
+      .then((FirebaseUser) {
+    print('novo usuário: sucesso! e-mail: ${FirebaseUser}');
+  }).catchError((erro) {
+    print('novo usuário: erro ${erro.toString()}');
+  });
 
   runApp(const MyApp());
 }
