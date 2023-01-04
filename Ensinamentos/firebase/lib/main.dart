24d85032b18d5ca1a35f7687f70db3a3ad3d40d5
IgorSSiqueira <igor.ss@live.com>
2023-01-04 09:00:33 -0300

    04/01/23 firebase outra versão

--- Ensinamentos/firebase/lib/main.dart
+++ Ensinamentos/firebase/lib/main.dart
@@ -1,17 +1,23 @@
 // @dart=2.9
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'dart:io';
 
 void main() async {
   //Inicializar Firebase
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
 
   //Firestore db = Firestore.instance;
+  //VERSÃO UM POUCO MAIS RECENTE (NÃO É A ÚLTIMA)
+  FirebaseFirestore db = FirebaseFirestore.instance;
 
-  /*db.collection('usuarios').document('002').setData({
+  db.collection('usuarios').doc('003').set({
     'nome': 'Ana Maria Silva',
     'idade': '25',
   });
@@ -19,8 +25,8 @@ void main() async {
       .collection('usuarios')
       .document('pontuacao')
       .setData({'Igor': '250', 'Ana': '590'});*/
-  
-    Adicionando com .add, cria a chave automaticamente.
+
+  /*Adicionando com .add, cria a chave automaticamente.
     Sendo então bom colocar a chave em uma variável para encontrar ela
   DocumentReference ref = await db.collection('noticias').add({
     'titulo': 'Ondas de calor em São Paulo',
@@ -133,14 +139,59 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  File _imagem;
+  String _statusUpload = 'Upload não iniciado';
+  String _urlImagemRecuperada = null;
+
   Future _recuperarImagem(bool daCamera) async {
+    File imagemSelecionada;
     if (daCamera) {
       //Pega da Camera
-      ImagePicker.pickImage(source: ImageSource.camera);
+      imagemSelecionada =
+          await ImagePicker.pickImage(source: ImageSource.camera);
     } else {
       //Pega da Galeria
-      ImagePicker.pickImage(source: ImageSource.gallery);
+      imagemSelecionada =
+          await ImagePicker.pickImage(source: ImageSource.gallery);
     }
+
+    setState(() {
+      _imagem = imagemSelecionada;
+    });
+  }
+
+  Future _uploadImagem() async {
+    //Referenciar arquivo
+    FirebaseStorage storage = FirebaseStorage.instance;
+    StorageReference pastaRaiz = storage.ref();
+    StorageReference arquivo = pastaRaiz.child('fotos').child('foto1.jpg');
+
+    //Fazer upload da imagem
+    StorageUploadTask task = arquivo.putFile(_imagem);
+
+    //Controlar progresso do upload
+    task.events.listen((StorageTaskEvent event) {
+      if (event.type == StorageTaskEventType.progress) {
+        setState(() {
+          _statusUpload = 'Em progresso';
+        });
+      } else if (event.type == StorageTaskEventType.success) {
+        _statusUpload = 'Upload realizado';
+      }
+    });
+
+    //Recuperar url da imagem
+    task.onComplete.then((StorageTaskSnapshot snapshot) {
+      _recuperarUrlImagem(snapshot);
+    });
+  }
+
+  Future _recuperarUrlImagem(StorageTaskSnapshot snapshot) async {
+    String url = await snapshot.ref.getDownloadURL();
+    print('resultado url: $url');
+    setState(() {
+      _urlImagemRecuperada = url;
+    });
   }
 
   @override
@@ -151,6 +202,7 @@ class _HomeState extends State<Home> {
       ),
       body: SingleChildScrollView(
         child: Column(children: [
+          Text(_statusUpload),
           ElevatedButton(
               onPressed: () {
                 _recuperarImagem(true);
@@ -161,6 +213,17 @@ class _HomeState extends State<Home> {
                 _recuperarImagem(false);
               },
               child: Text('Galeria')),
+          _imagem == null ? Container() : Image.file(_imagem),
+          _imagem == null
+              ? Container()
+              : ElevatedButton(
+                  onPressed: () {
+                    _uploadImagem();
+                  },
+                  child: Text('Upload image')),
+          _urlImagemRecuperada == null
+              ? Container()
+              : Image.network(_urlImagemRecuperada),
         ]),
       ),
     );
